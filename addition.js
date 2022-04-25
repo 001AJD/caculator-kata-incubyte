6c846ed1bb7ceb1a1b0ed568787879756cf682d6
001AJD <ajinkyadhomne.27@gmail.com>
2022-04-25 22:43:54 +0530

    refactored add function to handle empty string input

--- addition.js
+++ addition.js
@@ -1,8 +1,16 @@
 const add = number => {
+	// return invalid if input type is not string
 	if(typeof number !== 'string')
 	{
 		return 'invalid input'
 	}
+
+	// return 0 if input is empty string
+	if(number === '')
+	{
+		return 0;
+	}
+
 };
 
 export { add };
